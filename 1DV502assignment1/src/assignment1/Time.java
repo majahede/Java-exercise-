ae470efc16ee510ffbfd0f95d8cce57f2e9f68aa
Maja Hedegard <mh223pi@student.lnu.se>
2021-02-01 15:48:20 +0100

    add Time solution

--- 1DV502assignment1/src/assignment1/Time.java
+++ 1DV502assignment1/src/assignment1/Time.java
@@ -1,7 +1,19 @@
 package assignment1;
 
+import java.util.Scanner;
+
 public class Time {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Give a number of seconds: ");
+
+    int number = scan.nextInt();
+    int hours = number / 3600;
+    int minutes = (number % 3600) / 60;
+    int seconds = (number % 60);
     
+    System.out.println("This corresponds to: " + hours + " hours, " + minutes + " minutes and " + seconds + " seconds.");
+
+    scan.close();
   }
 }
