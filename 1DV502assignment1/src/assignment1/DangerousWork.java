47e2c42bb13ebbde90c93ba7a73c8f4f1eaeacfc
Maja Hedegard <mh223pi@student.lnu.se>
2021-02-01 20:55:48 +0100

    add DangerousWrok solution

--- 1DV502assignment1/src/assignment1/DangerousWork.java
+++ 1DV502assignment1/src/assignment1/DangerousWork.java
@@ -4,8 +4,26 @@ import java.util.Scanner;
 
 public class DangerousWork {
   public static void main(String[] args) {
-    /* For each day you stay at the work, the salary is doubled -- it will begin with 0.01 of a Swedish krona (also called 1 "öre") the first day and it will be 0.02 the second, 0.04 the third, 0.08 the fourth and so on. Write a program called DangerousWork.java that helps you calculate the minimum number of days you need to work to earn a specific amount. You do not need to validate the input, but you should keep in mind that you probably won't survive working more than 30 days... See an example of the program running below: */
+ 
     Scanner scan = new Scanner(System.in);
+    System.out.print("How much would you like to earn? ");
+    
+    int input = scan.nextInt();
+
+    double total = 0;
+    double salary = 0;
+    int workingDays = 0;
+
+    while (total <= input) { 
+        if (workingDays == 0) {
+          salary = 0.01;
+        }
+        total += salary;
+        workingDays++;
+        salary = salary * 2;
+      }
+
+    System.out.println("You will have your money in " + workingDays + " days.");
 
     scan.close();
   }
